2af2621d257f7126c0cb1abfff644c30831c8b33
Wellington <well.oliva@hotmail.com>
2021-03-01 22:26:32 -0300

    Introdução ao Javascript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Wellington Oliva";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toUpperCase());
+//alert(frase.replace("Japão", "Brasil"))
