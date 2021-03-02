7f34ebad324e1c3ec2835d6915345839ea2d9d45
Wellington <well.oliva@hotmail.com>
2021-03-01 23:59:29 -0300

    Testando exemplos de get

--- js/main.js
+++ js/main.js
@@ -1,11 +1,42 @@
-var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+/*
+var count;
+for (count=0; count <= 5; count++){
+    alert(count);
+};
+*/
+/* 
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt ("Qual sua idade?");
+if (idade >= 18){
+    alert ("maior de idade");
+}else{
+    alert ("menor de idade");
+}
+*/
+
+/*var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
 console.log(frutas);
 alert(frutas[1].nome);
-
+*/
 /*var fruta = {nome: "maça", cor: "vermelha"}
 console.log(fruta.nome);
 alert(fruta.cor);
 */
+
 //var lista = ["maça", "pêra", "laranja"];
 //lista.push("uva");
 //lista.pop();
