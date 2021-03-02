1c4557e1005b2f16ee191efeeb41a6d4eb477082
Wellington <well.oliva@hotmail.com>
2021-03-01 23:35:04 -0300

    Usando array e dicionário

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
-var nome = "Wellington Oliva";
-var n1 = 5;
-var n2 = 3;
-var frase = "Japão é o melhor time do mundo";
+var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+/*var fruta = {nome: "maça", cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+//var nome = "Wellington Oliva";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo";
 //alert(nome + " tem " + idade + " anos");
 //alert(idade + idade2);
-console.log(nome);
-console.log(n1 * n2);
-console.log(frase.toUpperCase());
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toUpperCase());
 //alert(frase.replace("Japão", "Brasil"))
